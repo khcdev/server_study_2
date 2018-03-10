0fdd175d8810d3f45cfa72c50ac436c576052a8a
chowhr1231 <thddbswo14@gmail.com>
2018-03-10 20:58:47 +0900

    version2_mysql connecting

--- song/Ex4/dbConfig.js
+++ song/Ex4/dbConfig.js
@@ -1,13 +1,4 @@
 const mysql = require('mysql');
 
-const con=mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '1234',
-    database : 'serverTest'
-});
 
-con.connect((err) => {
-    if(err) throw err;
-    console.log("Connectes!");
-})
+
--- song/Ex4/login.js
+++ song/Ex4/login.js
@@ -1,24 +1,39 @@
-const fs = require('fs');
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+const dbconfig={
+    host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'serverTest'
+};
+let con = mysql.createConnection(dbconfig);
 
-let data = fs.readFileSync("user.json");
-
-app.get('/', (req, res) =>{
-    res.send(<h1>Home page</h1>);
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>');
 });
 
-app.get('/login', (req, res, next)=>{
-    dishtml(req, res);
-    if(err){
-        next();
-    }
+app.get('/login',(req, res) => {
+    res.send('<h1>Log in Page</h1>');
 });
 
+app.post('/join', (req, res) => {
+    let user = {
+        'userid' : req.body.userid,
+        'userpw' : req.body.userpw,
+        'username' : req.body.username
+    };
 
+    var qury = con.query('INSERT INTO SETEST1 SET ?', user, (err, result) => {
+        if(err){
+            console.error(err);
+            throw err;
+        }
+        console.log(qury);
+        res.send(200, 'success');
+    });
+
+});
 
 app.listen(3000);
 
-function dishtml(req, res){
-    
-}
