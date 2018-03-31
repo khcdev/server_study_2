9ebdb58ef758186e1936de8fc61e7b408ae3d3bf
KangSeyun <dcp.k953@gmail.com>
2018-04-01 02:12:26 +0900

    seyun push ^^
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- kang/app/controller/auth.js
+++ kang/app/controller/auth.js
@@ -1,4 +1,4 @@
-var { User, Board } = require('../model');
+var { User } = require('../model');
 var hat = require('hat').rack();
 
 exports.authBasic = async (req, res) => {
@@ -16,8 +16,23 @@ exports.authBasic = async (req, res) => {
     });
 }
 
+exports.profile = async (req, res) => {
+    const apiKey = req.header['x-api-key'];
+    if(!apiKey) {
+        res.send(400, "require api key");
+    }
+
+    const user = User.where({token: apiKey}).fetch().
+    then((user) => {
+        res.send(200, 'valid')
+    }).catch((error)=> {
+        res.send(401, "not match api key")
+    })
+}
+
 exports.checkAuth = async (req, res) => {
     const apiKey = req.header['x-api-key'];
+
     if(!apiKey) {
         res.send(400, "require api key");
     }
@@ -41,8 +56,13 @@ exports.register = async (req, res) => {
     });
 
     try {
-        await user.save(null, {method: 'insert'});
-        res.send(200, {"token": "asdasdl1j23"})  
+        const response = await user.save(null, { method: 'insert' });
+        const key_duration = 3600 * 12;
+        const expires_in = Math.round(Date.now() / 1000) + key_duration;
+        const token = hat(expires_in);
+        response.set('token', token).save();
+        res.send(200, {"token": token})  
+        
     } catch (error) {
         res.send(400, error);  
     }
